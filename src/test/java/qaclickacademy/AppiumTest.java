65dc99a0b4cd13021e951d52f7f1cf04db1385f3
neerajbeniwal <neerajbeniwal15@gmail.com>
2021-08-19 09:37:56 +0530

    change made by neeraj

--- src/test/java/qaclickacademy/AppiumTest.java
+++ src/test/java/qaclickacademy/AppiumTest.java
@@ -12,6 +12,8 @@ public class AppiumTest {
 	@Test
 	public void IOSApps()
 	{
-		System.out.println("IOSApps");	
+		System.out.println("IOSApps");
+		System.out.println("IApps");	
+		System.out.println("AndApps");	
 	}
 }
